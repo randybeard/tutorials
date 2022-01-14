5cd87c679ebbc2986c913b58d831ef87b67a9044
beard <beard@byu.edu>
2022-01-14 15:08:48 -0700

    initial commit of splines

---
+++ splines/bspline_planner.py
@@ -0,0 +1,36 @@
+"""
+messing with b-splines
+        2/18/21 - RWB
+"""
+import numpy as np
+from scipy.interpolate import BSpline
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+order = 3  # order of spline polynomials
+knots = [0, 0, 0, 1, 2, 3, 3, 3]  # knot points
+ctrlpts = np.array([  # 3D control points
+    [0., 0., 0.],
+    [1., 0., 0.],
+    [1., 1., 0.],
+    [1., 1., 1.],
+    ])
+#c = [-1, 2, 0, -1]  # control points (1D for now)
+spl = BSpline(knots, ctrlpts, order)  # create spline
+t = np.linspace(0, 3, 100)
+spline_fine = spl(t)
+
+# plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+ax.plot(ctrlpts[:, 0], ctrlpts[:, 1], ctrlpts[:, 2],
+        '-o', label='definition nodes')
+ax.plot(spline_fine[:, 0], spline_fine[:, 1], spline_fine[:, 2],
+        'b', label='spline')
+ax.legend()
+ax.set_xlabel('x', fontsize=16, rotation=0)
+ax.set_ylabel('y', fontsize=16, rotation=0)
+ax.set_zlabel('z', fontsize=16, rotation=0)
+
+plt.show()
